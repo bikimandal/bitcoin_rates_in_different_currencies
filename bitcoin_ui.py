203faf7ac08ce7662e9e3c69110d16045ac89c30
Biki Mandal <108017848+bikimandal@users.noreply.github.com>
2022-07-12 22:37:52 +0530

    Update bitcoin_ui.py

--- bitcoin_ui.py
+++ bitcoin_ui.py
@@ -92,12 +92,6 @@ canvas.title("Bitcoin App")
 f = ("poppins", 15, "bold")
 t = ("poppins", 35, "bold")
 
-# default_currency_list = ["INR" , "USD" , "EUR" , "CNY" , "CAD" , "AUD"]
-# label3.config(text = str(default_currency_list))
-# label3 = tk.Label(canvas, font=t)
-# label3.pack()
-
-
 textField = tk.Entry(canvas, justify='center', width = 20, font = t)
 textField.pack(pady = 20)
 textField.focus()
